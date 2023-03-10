2e4a3566768ddd6f4bb3997f4bf7ef2a7fc11ef7
VMWareDaniil <paramonov.dr@phystech.edu>
2023-03-09 16:06:28 -0800

    authorization window

--- front-app/src/Components/Navbar.js
+++ front-app/src/Components/Navbar.js
@@ -1,28 +1,38 @@
 import React, { useState, useEffect } from 'react'
 import {ReactComponent as Logo} from '../logo.svg'
-import {NavLink, useMatch, useResolvedPath} from "react-router-dom"
+import {NavLink, useMatch, useNavigate, useResolvedPath} from "react-router-dom"
 import "./Navbar.css"
+import Modal from 'react-modal';
+import AuthorizationForm from "./AuthorizationForm"
+import "./Authorization.css"
 
+Modal.setAppElement('#root');
 
 export default function NavigationBar(props)
 {
-  const [data, setData] = useState([]);
-  
-  const apiGet = () => {
-    fetch("http://192.168.1.122:8080/static/kek.json")
-      .then((response) => response.json())
-      .then((json) => {
-      console.log(json);
-      setData(json);
-      });
-    };
+
+  const [user, setUser] = React.useState({
+    login: "",
+    password: "",
+    isLogin:false
+    });
+
+
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   let activeStyle = {
     textDecoration: "underline",
     textDecorationColor: "#E13737",
   }; 
 
+  function logInHandler()
+  {
+    setModalIsOpen(true);
+  }
+
 return (
+  <div>
   <nav className="Navbar">
     <NavLink exact to="/">
       <Logo className = "Navbar--logo"/>
@@ -52,8 +62,19 @@ return (
       </ul>
     </div>
     <div className="Navbar--auth">
-      <button onClick ={apiGet}>Login</button>
+      <button onClick ={logInHandler}>{user.isLogin ? user.login : "Login"}</button>
     </div>
   </nav>
+  <Modal 
+    isOpen={modalIsOpen} 
+    onRequestClose={() => setModalIsOpen(false)} 
+    className="Modal--container"
+    overlayClassName="Modal--overlay">
+  <AuthorizationForm 
+    modalHandler ={setModalIsOpen} 
+    userHandler={setUser} 
+    user = {user}/>
+  </Modal>
+  </div>
 );
 }
