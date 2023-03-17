86c943048d970297bd3bb2b8a567b8994ee4089c
VMWareDaniil <paramonov.dr@phystech.edu>
2023-03-17 05:52:33 -0700

    added fetch carousel

--- front-app/src/Components/MainPage/MainPage.js
+++ front-app/src/Components/MainPage/MainPage.js
@@ -1,14 +1,28 @@
 import React, { useEffect, useState } from 'react'
 import Carousel     from './Carousel'
 import FilmContainer from './FilmContainer'
-import dataCarousel from './data_carousel'
 import dataNews     from './data_news'
+import {BACKEND_URL} from '../config/Constants'
 import './MainPage.css'
 
 
 
 export default function MainPage()
 {
+    const [dataCarousel, setDataCarousel] = useState([]);
+
+    useEffect(() => {
+        fetch('http://192.168.31.100:8080/api/carousel')
+            .then(response => {
+                if (response.ok) {
+                    return response.json()
+                }
+            })
+            .then(data => {
+                console.log(data)
+                setDataCarousel(data);
+            })
+    }, []);
 
     return(
         <main>
--- front-app/src/Components/Navbar.js
+++ front-app/src/Components/Navbar.js
@@ -15,7 +15,7 @@ export default function NavigationBar(props)
     login: "",
     password: "",
     isLogin:false
-    });
+  });
 
 
 
